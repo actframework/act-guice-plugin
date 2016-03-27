9caa1bf59bf05a2061943adc6e69b5292d606187
Gelin Luo <greenlaw110@gmail.com>
2016-03-27 19:43:31 +1100

    remove reference to obsolete class: DaoInjectionListenerCache

--- src/main/java/act/di/guice/InjectionListenerAdaptor.java
+++ src/main/java/act/di/guice/InjectionListenerAdaptor.java
@@ -1,10 +1,5 @@
 package act.di.guice;
 
-import act.app.App;
-import act.db.Dao;
-import act.db.di.DaoInjectionListener;
-import act.db.di.DaoInjectionListenerCache;
-import act.di.DependencyInjectionListener;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.TypeLiteral;
@@ -12,23 +7,15 @@ import com.google.inject.matcher.Matchers;
 import com.google.inject.spi.InjectionListener;
 import com.google.inject.spi.TypeEncounter;
 import com.google.inject.spi.TypeListener;
-import org.osgl.$;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.List;
 
 public class InjectionListenerAdaptor implements Module, TypeListener {
     private GuiceDependencyInjector gdi;
 
-    private DaoInjectionListenerCache cache;
-
     InjectionListenerAdaptor(GuiceDependencyInjector gdi) {
         this.gdi = gdi;
-        this.cache = App.instance().service(DaoInjectionListenerCache.class);
-        if (null == this.cache) {
-            this.cache = new DaoInjectionListenerCache(App.instance());
-        }
     }
 
     @Override
